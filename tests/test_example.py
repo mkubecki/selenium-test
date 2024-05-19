12d7316a5fe6cc7ae37bb29b7ca96b207d79f766
Marcin Kubecki <33929613+mkubecki@users.noreply.github.com>
2024-05-19 19:21:14 +0200

    Add files via upload

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,6 +4,6 @@ from selenium.webdriver.common.by import By
 
 def test_example():
     driver = webdriver.Chrome()
-    driver.get("https://www.example.com")
+    driver.get("https://www.eduamp.pl")
     assert "Example Domain" in driver.title
     driver.quit()
--- tests/test_google.py
+++ tests/test_google.py
@@ -4,9 +4,6 @@ from selenium.webdriver.common.by import By
 
 def test_google_search():
     driver = webdriver.Chrome()
-    driver.get("https://www.google.com")
-    search_box = driver.find_element(By.NAME, "q")
-    search_box.send_keys("Selenium WebDriver")
-    search_box.submit()
+    driver.get("https://eduamp.pl")
     assert "Selenium WebDriver" in driver.title
     driver.quit()
